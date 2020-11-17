3f192839a84df1a1a8c97a731c9a7529d0a49100
Michael Devery <michaeldevery@gmail.com>
2020-11-17 20:23:33 +0000

    Updating request, fixing specific language request

--- src/Request/EventRequest.php
+++ src/Request/EventRequest.php
@@ -9,13 +9,53 @@ class EventRequest extends AbstractRequest
 {
     private DateTime $startRange;
     private DateTime $endRange;
-    private array $languages = [];
+    private array $languageCodes = [];
     private bool $getFeaturedOnly = false;
 
     public function __construct()
     {
         $this->startRange = new DateTime();
         $this->endRange = new DateTime('+3 days');
+    }
+
+    public function getStartRange(): DateTime
+    {
+        return $this->startRange;
+    }
+
+    public function setStartRange(DateTime $startRange): void
+    {
+        $this->startRange = $startRange;
+    }
+
+    public function getEndRange(): DateTime
+    {
+        return $this->endRange;
+    }
+
+    public function setEndRange(DateTime $endRange): void
+    {
+        $this->endRange = $endRange;
+    }
+
+    public function getLanguageCodes(): array
+    {
+        return $this->languageCodes;
+    }
+
+    public function setLanguageCodes(array $languageCodes): void
+    {
+        $this->languageCodes = $languageCodes;
+    }
+
+    public function isGetFeaturedOnly(): bool
+    {
+        return $this->getFeaturedOnly;
+    }
+
+    public function setGetFeaturedOnly(bool $getFeaturedOnly): void
+    {
+        $this->getFeaturedOnly = $getFeaturedOnly;
     }
 
     public function getPath(): string
@@ -28,7 +68,7 @@ class EventRequest extends AbstractRequest
         return [
             'start_range' => $this->formatDate($this->startRange),
             'end_range' => $this->formatDate($this->endRange),
-            'language_id' => $this->languages,
+            'language_ids' => implode(',', $this->languageCodes),
             'is_featured_only' => $this->getFeaturedOnly,
         ];
     }
