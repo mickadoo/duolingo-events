8b9f30677a1bf8d502bf8086c4b741d6438d07a6
Michael Devery <michaeldevery@gmail.com>
2021-05-03 15:36:43 +0100

    Use PHP intl instead of hardcoded list

--- src/Model/LanguageCodes.php
+++ src/Model/LanguageCodes.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Mickadoo\DuolingoEvents\Model;
 
+use Locale;
+use ResourceBundle;
 use RuntimeException;
 
 class LanguageCodes
@@ -65,134 +67,13 @@ class LanguageCodes
         );
     }
 
-    public static function getLanguageCodes()
+    public static function getLanguageCodes(): array
     {
-        return [
-            self::SPANISH,
-            self::JAPANESE,
-            self::ENGLISH,
-            self::ITALIAN,
-            self::PORTUGUESE,
-            self::GERMAN,
-            self::DUTCH,
-            self::FRENCH,
-            self::KOREAN,
-            self::CHINESE,
-            self::RUSSIAN,
-            self::HINDI,
-            self::ARABIC,
-            self::TURKISH,
-            self::LATIN,
-            self::SWEDISH,
-            self::GREEK,
-            self::IRISH,
-            self::POLISH,
-            self::NORWEGIAN,
-            self::HEBREW,
-            self::VIETNAMESE,
-            self::HAWAIIAN,
-            self::DANISH,
-            self::SCOTS_GAELIC,
-            self::HIGH_VALYRIAN,
-            self::INDONESIAN,
-            self::WELSH,
-            self::ROMANIAN,
-            self::CZECH,
-            self::SWAHILI,
-            self::FINNISH,
-            self::HUNGARIAN,
-            self::UKRAINIAN,
-            self::KLINGON,
-            self::NAVAJO,
-            self::ESPERANTO,
-            self::CATALAN,
-            self::GUARANI,
-        ];
+        return ResourceBundle::getLocales('');
     }
 
     public static function getDisplayNameForCode(string $code): string
     {
-        switch ($code) {
-            case self::SPANISH:
-                return 'Spanish';
-            case self::JAPANESE:
-                return 'Japanese';
-            case self::ENGLISH:
-                return 'English';
-            case self::ITALIAN:
-                return 'Italian';
-            case self::PORTUGUESE:
-                return 'Portuguese';
-            case self::GERMAN:
-                return 'German';
-            case self::DUTCH:
-                return 'Dutch';
-            case self::FRENCH:
-                return 'French';
-            case self::KOREAN:
-                return 'Korean';
-            case self::CHINESE:
-                return 'Chinese';
-            case self::RUSSIAN:
-                return 'Russian';
-            case self::HINDI:
-                return 'Hindi';
-            case self::ARABIC:
-                return 'Arabic';
-            case self::TURKISH:
-                return 'Turkish';
-            case self::LATIN:
-                return 'Latin';
-            case self::SWEDISH:
-                return 'Swedish';
-            case self::GREEK:
-                return 'Greek';
-            case self::IRISH:
-                return 'Irish';
-            case self::POLISH:
-                return 'Polish';
-            case self::NORWEGIAN:
-                return 'Norwegian';
-            case self::HEBREW:
-                return 'Hebrew';
-            case self::VIETNAMESE:
-                return 'Vietnamese';
-            case self::HAWAIIAN:
-                return 'Hawaiian';
-            case self::DANISH:
-                return 'Danish';
-            case self::SCOTS_GAELIC:
-                return 'Scottish Gaelic';
-            case self::HIGH_VALYRIAN:
-                return 'High Valyrian';
-            case self::INDONESIAN:
-                return 'Indonesian';
-            case self::WELSH:
-                return 'Welsh';
-            case self::ROMANIAN:
-                return 'Romanian';
-            case self::CZECH:
-                return 'Czech';
-            case self::SWAHILI:
-                return 'Swahili';
-            case self::FINNISH:
-                return 'Finnish';
-            case self::HUNGARIAN:
-                return 'Hungarian';
-            case self::UKRAINIAN:
-                return 'Ukrainian';
-            case self::KLINGON:
-                return 'Klingon';
-            case self::NAVAJO:
-                return 'Navajo';
-            case self::ESPERANTO:
-                return 'Esperanto';
-            case self::CATALAN:
-                return 'Catalan';
-            case self::GUARANI:
-                return 'Guarani';
-        }
-
-        throw new RuntimeException(sprintf('Cannot find language code "%s"', $code));
+        return Locale::getDisplayName($code);
     }
 }
